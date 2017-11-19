c7f3a794b994718fec18833311493142ec47cca7
後閑諒一 <nargok@gmail.com>
2017-11-19 19:38:07 +0900

    create 2 models

---
+++ app/src/main/kotlin/sample/qiitaclient/model/Article.kt
@@ -0,0 +1,9 @@
+package sample.qiitaclient.model
+
+/**
+ * Created by houxianliangyi on 2017/11/19.
+ */
+data class Article(val id: String,
+                   val title: String,
+                   val url: String,
+                   val user: User)
--- app/src/main/kotlin/sample/qiitaclient/model/User.kt
+++ app/src/main/kotlin/sample/qiitaclient/model/User.kt
@@ -3,3 +3,6 @@ package sample.qiitaclient.model
 /**
  * Created by houxianliangyi on 2017/11/19.
  */
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl: String)
